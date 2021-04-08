4cfd7717448958490347b613d4270f158aa86124
david-salac <info@davidsalac.eu>
2021-04-08 07:53:00 +0100

    Updates in Portable Spreadsheet article

--- generator/article_portable_spreadsheet.py
+++ generator/article_portable_spreadsheet.py
@@ -2,9 +2,9 @@
 import datetime
 import crinita as cr
 
-lead = """One of the typical challenges when doing export of numerical computations is how to include the way how you achieve the results. Typically if results should be in Excel sheet at the end, you want to see the computation formula if you click on each cell. There is a native way using some Excel driver (like XlsxWriter) available in Python, but if you are dealing with a big Excel sheet, it becomes quite inconvenient."""
+lead = """One of the typical challenges when doing the export of numerical computations is how to include the way your computations were achieved. Typically, if results should be in an Excel sheet in the end, you want to see a computation formula if you click on each cell. There is a native method - using some Excel driver (like XlsxWriter) available in Python. But if you are dealing with a big Excel sheet (or multiple sheets in one file), this approach becomes quite inconvenient. We present a new library called Portable Spreadsheet - it can easily export simply defined formulas in Python to many formats including Excel, JSON, etc."""
 
-content = """One of the typical challenges when doing export of numerical computations is how to include the way how you achieve the results. Typically if results should be in Excel sheet at the end, you want to see the computation formula if you click on each cell. There is a native way using some Excel driver (like XlsxWriter) available in Python, but if you are dealing with a big Excel sheet, it becomes quite inconvenient. This article is about a new library called Portable Spreadsheet that can easily overcome these problems.
+content = """One of the typical challenges when doing the export of numerical computations is how to include the way your computations were achieved. Typically, if results should be in an Excel sheet in the end, you want to see a computation formula if you click on each cell. There is a native method - using some Excel driver (like XlsxWriter) available in Python. But if you are dealing with a big Excel sheet (or multiple sheets in one file), this approach becomes quite inconvenient. We present a new library called Portable Spreadsheet - it can easily export simply defined formulas in Python to many formats including Excel, JSON, etc.
 <h2>Simple problem</h2>
 <p>Say that you have an e-shop written in Python and want to export some simple Excel sheet that summarizes revenues per each category. There can be just two columns, one representing the category and another one representing revenues. </p> 
 
@@ -31,10 +31,14 @@ worksheet.write(0, 1, food_rev)
 worksheet.write(1, 1, drinks_rev)
 worksheet.write(2, 1, headphones_rev)
 # Write results
-worksheet.write_formula(3, 1, "=SUM(B1:B3)",
-                        value=(food_rev + drinks_rev + headphones_rev))
-worksheet.write_formula(4, 1, "=MIN(B1:B3)",
-                        value=min(food_rev, drinks_rev, headphones_rev))
+worksheet.write_formula(
+    3, 1, "=SUM(B1:B3)", 
+    value=(food_rev + drinks_rev + headphones_rev)
+)
+worksheet.write_formula(
+    4, 1, "=MIN(B1:B3)",
+    value=min(food_rev, drinks_rev, headphones_rev)
+)
 worksheet.write_formula(5, 1, "=B1+B2",
                         value=food_rev + drinks_rev)
 # Close the sheet (in order to write it to the file)
@@ -49,20 +53,24 @@ workbook.close()
 <pre class="code"><code>import portable_spreadsheet as ps
 
 # Create the spreadsheet with the correct labels
-sheet = ps.Spreadsheet.create_new_sheet(
+sheet = ps.Sheet.create_new_sheet(
     6, 1,
     rows_labels=['Food', 'Drink', 'Headphones', 'Total', 'Minimal', 'Grocery'],
     columns_labels=['Revenue']
 )
-# Set the values
+# Set the values (in the Pandas-like way)
 sheet.loc['Food', 'Revenue'] = 15000
 sheet.loc['Drink', 'Revenue'] = 16000
 sheet.loc['Headphones', 'Revenue'] = 1000
 # Set the computations
-sheet.loc['Total', 'Revenue'] = sheet.loc['Food':'Total', 'Revenue'].sum()
-sheet.loc['Minimal', 'Revenue'] = sheet.loc['Food':'Total', 'Revenue'].min()
-sheet.loc['Grocery', 'Revenue'] = sheet.loc['Food', 'Revenue'] + \
-                                  sheet.loc['Drink', 'Revenue']
+sheet.loc['Total', 'Revenue'] = \
+    sheet.loc['Food':'Total', 'Revenue'].sum()
+sheet.loc['Minimal', 'Revenue'] = \ 
+    sheet.loc['Food':'Total', 'Revenue'].min()
+sheet.loc['Grocery', 'Revenue'] = (
+        sheet.loc['Food', 'Revenue'] + 
+        sheet.loc['Drink', 'Revenue']
+    )
 # Export result to Excel
 sheet.to_excel("rev.xlsx")
 # Export result to JSON
@@ -83,6 +91,11 @@ json = sheet.to_json()</code></pre>
 <p>In the picture above, you can see prefixes and suffixes. Some prefixes and suffixes are empty sets. This picture is just an illustration, but it shows how each word can be constructed. All these prefixes and suffixes (and other rules) are defined as the grammars for each language in a separate file.</p>
 <p>The rest of the Portable Spreadsheet library is simple storing of words for each cell defined by its coordinates. Each word (like that one with the sum mentioned above) can be the part of the other word (acting as an operand). This approach allows defining grammars for each language that should be part of the export (like native language description, Python language, Excel).</p>
 
+<h2>The new version of Portable Spreadsheet</h2>
+<p>There is also a new version of Portable Spreadsheet now available. It contains many enhanced features (like the possibility of speed optimization). But the main new feature is the support of outputs with multiple sheets - called Workbook. The interface for using the workbook is very intuitive - it is a container for sheets (with implemented functionality for exporting). Another enhancement is the possibility to have a custom-defined variables sheet - that is a sheet that defines variables for computations in other sheets (you can add as many columns and rows as needed with your content).</p> 
+<p>Regarding performance, the most important new feature enables you to create a sheet with values only (it means, you cannot export to Excel/JSON and other formats afterwards). That makes computations much faster. You just need to recompute all the values with proper configuration when you need them. Also, there is now a possibility to reduce the number of languages in which words are constructed - so for example, if you want to export only to Excel, you do not need to waste your computational time with other formats (this feature makes library slightly faster as well). </p>
+<p>Alas, the only way how to improve the Portable Spreadsheet library was to use an application interface that is not backwards compatible in a few ways (for example, Spreadsheet class was renamed to Sheet). There are also many new classes available (that allows the creation of outputs with multiple sheets in them). Many bugs were fixed as well (like the bug in shape property that now works correctly).</p>
+
 <h2>Summary </h2>
 <p>The purpose of this article is to present a new library for Python called Portable Spreadsheet (pip install portable-spreadsheet) that allows you to easily export computation formulas in a spreadsheet to various type of formats (mainly Excel and JSON). So you can see how each computation in a spreadsheet is done without manually coding each cell. </p>
 """
@@ -100,5 +113,5 @@ ENTITY = cr.Article(
           cr.Tag('Excel', 'excel')],
     content=content,
     lead=lead,
-    description="Portable Spreadsheet is the Python library encapsulating all the arithmetical operation needed to creates descriptive spreadsheets exportable to Excel or JSON."  # noqa: E501
+    description="We present a new library called Portable Spreadsheet - it can easily export simply defined formulas in Python to many formats including Excel, JSON, etc."  # noqa: E501
 )
